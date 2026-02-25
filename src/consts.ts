f4f2ae0e25b17534074ab94e31e3607937df4312
Las Cositas de Sita <patricia@lascositasdesita.com>
2026-02-25 14:50:30 +0100

    feat: improve SEO metadata and responsive typography

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Blog Las Cositas de Sita';
-export const SITE_DESCRIPTION = 'Blog de Las Cositas de Sita';
+export const SITE_DESCRIPTION = 'En este blog encontrarás posts sobre diseño, edición de vídeo, automatizaciones, herramientas y no-code.';
