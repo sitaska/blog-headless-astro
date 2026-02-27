b510178d2a9c64c4e2c9c0d5321422b2e47ddfb0
Las Cositas de Sita <patricia@lascositasdesita.com>
2026-02-27 18:23:24 +0100

    Corrige favicon y logo RSS del blog

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -2,16 +2,15 @@ import rss from '@astrojs/rss';
 import { SITE_DESCRIPTION, SITE_TITLE } from '../consts';
 import { getEntityPath, getPosts, stripHtml } from '../lib/wp';
 
-const FEED_IMAGE_URL =
-	'https://cms.lascositasdesita.com/wp-content/uploads/2026/01/cropped-sita-logo-circle.png';
-
 export async function GET(context) {
 	const posts = await getPosts();
+	const siteUrl = new URL('/', context.site).toString();
+	const feedImageUrl = new URL('/favicon.png', context.site).toString();
 	return rss({
 		title: SITE_TITLE,
 		description: SITE_DESCRIPTION,
 		site: context.site,
-		customData: `<image><url>${FEED_IMAGE_URL}</url><title>${SITE_TITLE}</title><link>${context.site}</link></image>`,
+		customData: `<image><url>${feedImageUrl}</url><title>${SITE_TITLE}</title><link>${siteUrl}</link></image>`,
 		items: posts.map((post) => ({
 			title: stripHtml(post.title.rendered),
 			description: stripHtml(post.excerpt.rendered),
